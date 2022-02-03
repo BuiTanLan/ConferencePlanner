830d1f89164e028eed3e8d70dc1f078c0829ba6f
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2022-02-03 23:34:31 +0700

    Add project files.

---
+++ GraphQL/Query.cs
@@ -0,0 +1,27 @@
+﻿namespace GraphQL
+{
+    public class Query
+    {
+        public Book GetBook() =>
+            new Book
+            {
+                Title = "C# in depth.",
+                Author = new Author
+                {
+                    Name = "Jon Skeet"
+                }
+            };
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public string Name { get; set; }
+    }
+}
